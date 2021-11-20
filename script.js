964caa77600ebabd0cd6f2967433884b863c6102
Tri Nguyen <tri.nguyen1086@gmail.com>
2021-11-20 14:19:32 -0800

    add more special characters to var symbol

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.querySelector('#generate').addEventListener('click', writePassword);
 
 // List all possible outcomes.
 var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var symbol = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';'];
+var symbol = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
 var alphalower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var alphaupper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
